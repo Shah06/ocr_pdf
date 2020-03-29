f46bed55bd172c2cfccae61c9418793259bf2102
Atharva Shah <atharvashah123@gmail.com>
2020-03-28 20:43:50 -0700

    Added feature to pass in input and output files using command line arguments. Script also cleans up better

--- makepdf.py
+++ makepdf.py
@@ -1,10 +1,35 @@
 import pdf2image
 import PIL
 import os
+import argparse
 
-INPUT_FILE = 'test.pdf'
+# TODO for the future:
+# store files temporarily in /tmp/
+
+parser = argparse.ArgumentParser(description='Simple script to make PDFs searchable')
+parser.add_argument(
+    'input_file',
+    help='filepath for PDF to be targeted'
+)
+parser.add_argument(
+    '-o',
+    '--output',
+    default='out',
+    help='output filepath'
+)
+
+args = parser.parse_args()
+
+# do stuff with parsed args here:
+
+INPUT_FILE = args.input_file
 SAVE_PATH = ''
 OUTPUT_FILE = 'out'
+# if fpath contains a .pdf at the end, remove it. tesseract does this automatically
+fpath = os.path.splitext(args.output)[0]
+if os.path.exists(fpath):
+    OUTPUT_FILE = os.path.basename(fpath)
+    SAVE_PATH = os.path.dirname(fpath)
 
 pages = pdf2image.convert_from_path(INPUT_FILE)
 ocr_command = open('ocr_command', 'w')
@@ -19,7 +44,7 @@ for page in pages:
     counter += 1
 
 # execute tesseract command
-command = 'tesseract ocr_command '+OUTPUT_FILE+' -l eng pdf'
+command = 'tesseract ocr_command '+fpath+' -l eng pdf'
 ocr_command.close()
 print('Executing "' + command + '"')
 os.system(command)
@@ -31,4 +56,4 @@ for line in lines:
     print('Deleting ' + line)
     os.remove(line)
 pdf_files.close()
-#os.remove('ocr_command')
+os.remove('ocr_command')
